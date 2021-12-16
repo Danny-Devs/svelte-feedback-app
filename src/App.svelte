a3b1233212ccd0ce259c165081982d9e507b67c0
Daniel Ahn <et3rnal.d@gmail.com>
2021-12-16 15:24:02 -0800

    initial commit traversy YT svelte crash course 2021

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import FeedbackForm from './components/FeedbackForm.svelte'
+  import FeedbackList from './components/FeedbackList.svelte'
+  import FeedbackStats from './components/FeedbackStats.svelte'
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, reprehenderit. Odit ut, veniam error praesentium asperiores beatae quod possimus voluptatem placeat voluptates inventore fuga quisquam.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, reprehenderit. Odit ut, veniam error praesentium asperiores beatae quod possimus voluptatem placeat voluptates inventore fuga quisquam.',
+    },
+    {
+      id: 3,
+      rating: 5,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, reprehenderit. Odit ut, veniam error praesentium asperiores beatae quod possimus voluptatem placeat voluptates inventore fuga quisquam.',
+    },
+  ]
+
+  $: count = feedback.length
+  $: average =
+    feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail
+    feedback = feedback.filter((item) => item.id !== itemId)
+  }
+</script>
+
+<main class="container">
+	<FeedbackForm />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
+
+<style>
+</style>
---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,66 @@
+<script>
+  import Card from './Card.svelte'
+  import Button from './Button.svelte'
+  import RatingsSelect from './RatingSelect.svelte'
+
+  let text = ''
+  let rating = 10
+  let btnDisabled = true
+  let min = 10
+  let message
+
+  const handleInput = () => {
+    if (text.trim().length <= min) {
+      message = `Text must be at least ${min} characters`
+      btnDisabled = true
+    } else {
+      message = null
+      btnDisabled = false
+    }
+  }
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate our service with us?</h2>
+  </header>
+  <form>
+    <RatingsSelect />
+    <div class="input-group">
+      <input
+        type="text"
+        on:input={handleInput}
+        bind:value={text}
+        placeholder="Tell us about what keeps you coming back"
+      />
+      <Button disabled={btnDisabled} type="submit">Send</Button>
+    </div>
+    {#if message}
+      <div class="message">
+        {message}
+      </div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+    text-align: center;
+    margin-bottom: 15px;
+  }
+
+  .input-group {
+    display: flex;
+    position: relative;
+    align-items: center;
+  }
+
+  input {
+    width: 100%;
+    padding: 16px 12px;
+    border-radius: 4px;
+    border: 1px solid #333;
+  }
+</style>
